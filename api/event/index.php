6c453a7d18be3453dfa33ce3351b9ee129ab9418
ijontychie <baldauf.a@berlin.gcm.schule>
2026-07-01 09:32:04 +0200

    eventapi

--- api/event/index.php
+++ api/event/index.php
@@ -1,8 +1,42 @@
 <?php
 
+// 1. Alle Fehlerarten berichten (inkl. zukünftiger Levels)
+error_reporting(E_ALL);
+
+// 2. Fehler im Browser anzeigen
+ini_set('display_errors', 1);
+
+// 3. Auch Fehler anzeigen, die beim PHP-Start auftreten (wichtig für Parse-Fehler)
+ini_set('display_startup_errors', 1);
+
 require_once("../../lib/Database.php");
 
 $database = new Database();
+
+
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case "GET":
+        get_request_handler($_GET);
+        //echo "GET";
+        break;
+    case "POST":
+        post_request_handler($_POST);
+        echo "POST";
+        break;
+    default:
+        http_response_code(405); // Wichtig für korrekte API-Antworten
+        header('Allow: GET, POST'); // Informiert den Client über erlaubte Methoden
+        echo "Methode nicht erlaubt";
+        break;
+}
+
+
+
+
+
+
+
+
 
 
 function get_request_handler($query) {
@@ -14,19 +48,24 @@ function get_request_handler($query) {
     echo json_encode($result);
 }
 
-function post_request_handler($_POST){
 
-    $query = tidyup($_POST);
+
+function post_request_handler($query){
+
+    //$query = tidyup($_POST);
 
     $result = array();
     $result[0]['debug'] = 'Lege ein neues Event an oder ändere die Daten.';
 
     global $database;
 
-    if (isset($query['id']){
+    if (isset($query['id'])){
         $event = $database->query("SELECT * FROM events WHERE id = ?",array($query['id']));
-    }else{
-        $event = $database->query("SELECT * FROM events");
+        //echo'there is an id';
+    }
+    else{
+        //$event = $database->query("SELECT * FROM events");
+        echo'there is NO id';
     }
 
     $result[0]['db-result'] = $event;
@@ -34,29 +73,4 @@ function post_request_handler($_POST){
     echo json_encode($result);
 
 
-}
-
-
-function tidyup($compromised){
-    // Entfernt Strings, die SQL-Keywords enthalten
-    $cleanArray = array_filter($sqlArray, function($value) {
-        $sqlKeywords = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'ALTER'];
-        foreach ($sqlKeywords as $keyword) {
-            if (stripos($value, $keyword) !== false) {
-                return false; // Entfernen, wenn Keyword gefunden
-            }
-        }
-        return true; // Behalten, wenn kein Keyword gefunden
-    });
-    return $cleanArray;
-}
-
-
-
-switch ($_SERVER["REQUEST_METHOD"]) {
-    case "GET":
-        get_request_handler($_GET); break;
-    case "POST":
-        post_request_handler($_POST); break;
-    default: echo "";
 }
